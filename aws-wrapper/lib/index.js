e8d9363be5f2259665adc146f813dde8a17df3ea
wettingchou <wtchou1209@gmail.com>
2016-06-14 23:53:03 +0800

    untrack aws-wrapper

--- aws-wrapper/lib/index.js
+++
@@ -1,9 +0,0 @@
-var S3Uploader = require('./S3Uploader');
-var S3Remover = require('./S3Remover');
-var signer = require('./signer');
-
-module.exports = {
-  S3Uploader: S3Uploader,
-  S3Remover: S3Remover,
-  signer: signer
-}
