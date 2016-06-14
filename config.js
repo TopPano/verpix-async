493516d3ea30594351b47086c23a9906c3b70a39
wettingchou <wtchou1209@gmail.com>
2016-06-14 14:31:11 +0800

    rename enviornment variable SERVERS to G_SERVERS in order to be name consistency with the variable in API server

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  servers: process.env.SERVERS ? [ JSON.parse(process.env.SERVERS) ] : [ { host: 'localhost', port: 4730 } ],
+  servers: process.env.G_SERVERS ? [ JSON.parse(process.env.G_SERVERS) ] : [ { host: 'localhost', port: 4730 } ],
   defaultTimeout: 30
 };
